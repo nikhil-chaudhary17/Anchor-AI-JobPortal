e016600a10098c58f99d10c0d3235bcad50b1c92
Nikhil Chaudhary <nikhilchaudhary000111@gmail.com>
2026-09-16 00:04:58 +0530

    Fix authentication cookie settings

--- backend/server.js
+++ backend/server.js
@@ -32,7 +32,7 @@ app.use(morgan("dev"));
 app.use(helmet());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: process.env.CLIENT_URL,
     credentials: true,
   }),
 );
--- backend/utils/generateToken.js
+++ backend/utils/generateToken.js
@@ -1,22 +1,20 @@
-import jwt from 'jsonwebtoken'
+import jwt from "jsonwebtoken";
 
-const  generateToken = (res , userId) => {
-    const token = jwt.sign(
-        {userId},
-        process.env.JWT_SECRET,
-        {expiresIn: "7d"}
-    )
+const generateToken = (res, userId) => {
+  const token = jwt.sign({ userId }, process.env.JWT_SECRET, {
+    expiresIn: "7d",
+  });
 
-    //store in cookie
+  //store in cookie
 
-    res.cookie("token" , token, {
-        httpOnly: true,
-        secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
-        maxAge: 7 * 24 * 60 * 60 * 1000
-    })
+  res.cookie("token", token, {
+    httpOnly: true,
+    secure: process.env.NODE_ENV === "production",
+    sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+    maxAge: 7 * 24 * 60 * 60 * 1000,
+  });
 
-    return token
-}
+  return token;
+};
 
-export default generateToken; 
+export default generateToken;
